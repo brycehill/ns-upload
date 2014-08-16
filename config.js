0e4c6089ab6ac2bcf06a8612fa812beebb647470
Bryce Hill <bryceahill@gmail.com>
2014-08-15 20:25:00 -0700

    Add empty config file

---
+++ config.js
@@ -0,0 +1,14 @@
+var Config = {
+    url: '',
+    // Root path to project in the file cabinet
+    nsRootPath: '',
+    watched: '',
+    auth: {
+        email: '',
+        pass: '',
+        account: '',
+        role: ''
+    }
+}
+
+module.exports = Config
