6c89c9e9f484f4c4c1bbf68c58a71ac35bd237ee
Bryce Hill <bryceahill@gmail.com>
2016-04-01 16:31:02 -0700

    Add .editorconfig and comply \#fff/s/

--- index.js
+++ index.js
@@ -14,71 +14,71 @@ var success = chalk.green
 var message = chalk.cyan
 
 var app = new Liftoff({
-    name: 'ns-upload',
-    configName: 'ns-upload'
+  name: 'ns-upload',
+  configName: 'ns-upload'
 })
 
 app.launch({ cwd: argv.cwd }, run)
 
 function run(env) {
-    if (!env.configPath) {
-        util.error(error('No configuration file found'))
-        process.exit(1)
-    }
+  if (!env.configPath) {
+    util.error(error('No configuration file found'))
+    process.exit(1)
+  }
 
-    var config = require(env.configPath)
+  var config = require(env.configPath)
 
-    chokidar.watch(config.watched).on('change', function getContents(path) {
-        var re = /(.[^\/]*)\//g,
-            fileCabinetPath = path.replace(process.cwd() + '/', ''),
-            folders = fileCabinetPath.match(re),
-            slash = path.lastIndexOf('/'),
-            fileName = (!folders) ? fileCabinetPath : path.substr(slash + 1),
-            type = mime.lookup(fileName),
-            auth = config.auth
+  chokidar.watch(config.watched).on('change', function getContents(path) {
+    var re = /(.[^\/]*)\//g,
+      fileCabinetPath = path.replace(process.cwd() + '/', ''),
+      folders = fileCabinetPath.match(re),
+      slash = path.lastIndexOf('/'),
+      fileName = (!folders) ? fileCabinetPath : path.substr(slash + 1),
+      type = mime.lookup(fileName),
+      auth = config.auth
 
-            util.log(message(util.format('%s was changed', fileName)))
-            fs.readFile(path, 'utf8', uploadFile)
+      util.log(message(util.format('%s was changed', fileName)))
+      fs.readFile(path, 'utf8', uploadFile)
 
-            function uploadFile(err, content) {
-                if (err) util.error(error('ERROR: ', err))
-                util.log(message(util.format('Uploading %s', fileName)))
-                request({
-                    url: config.url,
-                    method: 'put',
-                    headers: {
-                        'User-Agent-x': 'SuiteScript-Call',
-                        'Content-Type': 'application/json',
-                        'Content-Language': 'en-US',
-                        'Authorization': 'NLAuth nlauth_account='+auth.account+', nlauth_email='+auth.email+', nlauth_signature='+auth.pass+', nlauth_role='+auth.role
-                    },
-                    body: JSON.stringify({
-                        fileName: fileName,
-                        content: content,
-                        path: config.nsRootPath + (folders ? folders.join('') : '') + fileName,
-                        fileType: m2NS(type)
-                    })
-                }, parseNSResponse)
+      function uploadFile(err, content) {
+        if (err) util.error(error('ERROR: ', err))
+        util.log(message(util.format('Uploading %s', fileName)))
+        request({
+          url: config.url,
+          method: 'put',
+          headers: {
+            'User-Agent-x': 'SuiteScript-Call',
+            'Content-Type': 'application/json',
+            'Content-Language': 'en-US',
+            'Authorization': 'NLAuth nlauth_account='+auth.account+', nlauth_email='+auth.email+', nlauth_signature='+auth.pass+', nlauth_role='+auth.role
+          },
+          body: JSON.stringify({
+            fileName: fileName,
+            content: content,
+            path: config.nsRootPath + (folders ? folders.join('') : '') + fileName,
+            fileType: m2NS(type)
+          })
+        }, parseNSResponse)
+      }
+
+      function parseNSResponse(err, res, body) {
+        try {
+          body = JSON.parse(body)
+          if (body.error) {
+            if (body.error.code === 'RCRD_DSNT_EXIST') {
+                // Try to upload clean file?
             }
-
-            function parseNSResponse(err, res, body) {
-                try {
-                    body = JSON.parse(body)
-                    if (body.error) {
-                        if (body.error.code === 'RCRD_DSNT_EXIST') {
-                            // Try to upload clean file?
-                        }
-                        /* Potential error codes:
-                        SSS_MISSING_REQD_ARGUMENT - Missing a body value
-                        RCRD_DSNT_EXIST - missing record in netsuite */
-                        util.error(error('Error Code:', body.error.code, '\nMessage:', body.error.message))
-                    } else {
-                        util.log(success('File ' + fileName + ' Uploaded Successfully'))
-                    }
-                } catch(e) {
-                    util.error(error('Exception caught.', e.message))
-                }
-            }
+            /* Potential error codes:
+            SSS_MISSING_REQD_ARGUMENT - Missing a body value
+            RCRD_DSNT_EXIST - missing record in netsuite */
+            util.error(error('Error Code:', body.error.code, '\nMessage:', body.error.message))
+          } else {
+            util.log(success('File ' + fileName + ' Uploaded Successfully'))
+          }
+        } catch(e) {
+          util.error(error('Exception caught.', e.message))
         }
-    )   
+      }
+    }
+  )
 }
